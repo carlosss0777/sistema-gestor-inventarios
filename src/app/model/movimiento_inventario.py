496f8a856e6fd00c3d7288d570da44d146117d75
Carito-code <as21020@ues.edu.sv>
2026-04-29 21:21:12 -0600

    Fix MovimientoInventario
    Se corrige la clase.

--- src/app/model/movimiento_inventario.py
+++ src/app/model/movimiento_inventario.py
@@ -11,37 +11,21 @@ class MovimientoInventario:
     def nombre_producto(self):
         return self._nombre_producto
 
-    @nombre_producto.setter
-    def nombre_producto(self, valor):
-        self._nombre_producto = valor
 
     # Tipo de movimiento
     @property
     def tipo_movimiento(self):
         return self._tipo_movimiento
 
-    @tipo_movimiento.setter
-    def tipo_movimiento(self, valor):
-        if valor.lower() not in ["entrada", "salida"]:
-            raise ValueError("El tipo de movimiento debe ser 'entrada' o 'salida'")
-        self._tipo_movimiento = valor.lower()
 
     # Cantidad
     @property
     def cantidad(self):
         return self._cantidad
 
-    @cantidad.setter
-    def cantidad(self, valor):
-        if valor <= 0:
-            raise ValueError("La cantidad debe ser mayor que 0")
-        self._cantidad = valor
 
     # Fecha
     @property
     def fecha(self):
         return self._fecha
-
-    @fecha.setter
-    def fecha(self, valor):
-        self._fecha = valor
+    
