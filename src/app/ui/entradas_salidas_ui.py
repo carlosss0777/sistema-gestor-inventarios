1de48dcb3d30679903e29be2a0f7b3563962e7ab
Carlos Ayala <am25008@ues.edu.sv>
2026-05-16 14:47:44 -0600

    feat: metodo para registrar entrada añadido

--- src/app/ui/entradas_salidas_ui.py
+++ src/app/ui/entradas_salidas_ui.py
@@ -1,9 +1,17 @@
 # Modulo de registro movimientos de stock
 
+from app.service.movimiento_service import movimiento_service
+from app.service.producto_service import ProductoService
+from app.ui.producto_ui import producto_ui
+from datetime import datetime
 from app.utils.tools import *
 
 class entradas_salidas_ui:
-    
+    def __init__(self, movimiento_service: movimiento_service, producto_service: ProductoService, producto_ui: producto_ui):
+        self.movimiento_service = movimiento_service
+        self.producto_service = producto_service
+        self.producto_ui = producto_ui
+        
     def menu_es(self):
         while True:
             while True:
@@ -26,7 +34,7 @@ class entradas_salidas_ui:
             match opcion:
                 case 1:
                     limpiar_pantalla()
-                    continue
+                    self.registrar_entrada()
                 
                 case 2:
                     limpiar_pantalla()
@@ -40,3 +48,35 @@ class entradas_salidas_ui:
                 case _:
                     print("\nOpción no válida. Intenta nuevamente...")
                     pausa()
+                    
+    def registrar_entrada(self):
+        limpiar_pantalla()
+        print("\n------------------------------------------------------------")
+        print("               -- REGISTRAR ENTRADA --")
+        print("------------------------------------------------------------")
+        productos = self.producto_service.get_productos()
+        
+        producto = self.producto_ui._seleccionar_producto("Selecciona el producto a editar (0 para cancelar): ")
+        if producto is None:
+            pausa()
+            return
+        
+        nombre_producto = producto.nombre
+        cantidad = self._pedir_cantidad()
+        tipo_movimiento = "Entrada"
+        fecha_hora = datetime.now()
+        self.producto_service.cambiar_stock(producto, cantidad, tipo_movimiento)
+        self.movimiento_service.registrar_movimiento(producto, tipo_movimiento, cantidad, fecha_hora)
+        
+        print("\nMovimiento registrado correctamente.")
+        detener()
+        
+    def _pedir_cantidad(self):
+        while True:
+            cantidad = input("Ingresa la cantidad a añadir: ")
+            
+            try:
+                return self.movimiento_service.validar_cantidad(cantidad)
+            except ValueError as e:
+                mensaje_error(e)
+                pausaLarga()
