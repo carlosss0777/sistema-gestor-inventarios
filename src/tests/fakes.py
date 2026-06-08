e7018496e7aee7d588c327bfdd3bcc75f712c882
Carlos Ayala <am25008@ues.edu.sv>
2026-06-07 18:17:03 -0600

    feat: se añaden repositories falsos que simulan las clases de la capa Repository

--- src/tests/fakes.py
+++ src/tests/fakes.py
@@ -1,7 +1,12 @@
 # Repositories falsos compartidos para todas las pruebas
 
+"""
+Repositories falsos para pruebas unitarias. Estos repositorios no interactúan con archivos ni datos, sino que simulan el comportamiento esperado para facilitar las pruebas de los servicios (necesarios porque todos los servicios reciben en el constructor el repository, si no se colocan, dara error).
+"""
 class ProductoRepositoryFalso:
+    # Metodo guardar() no hace nada, solo simula el comportamiento de guardar una lista de productos
     def guardar(self, lista): pass
+    # Metodo cargar() devuelve una lista vacia, simulando que no hay productos guardados
     def cargar(self, proveedores): return []
 
 class ProveedorRepositoryFalso:
