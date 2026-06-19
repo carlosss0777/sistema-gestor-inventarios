079f96bec6d4f333a10142ee4b6d1d551d7b927d
BrandonGomezUES <GM21057@ues.edu.sv>
2026-06-19 11:46:10 -0600

    test(proveedor): verificar que validar_nombre elimine espacios en blanco

--- src/tests/test_proveedor_service.py
+++ src/tests/test_proveedor_service.py
@@ -11,10 +11,20 @@ class TestProveedorService(unittest.TestCase):
     # python -m unittest tests.test_proveedor_service
     
     def setUp(self):
-        self.service = ProveedorService(ProveedorRepositoryFalso())
+        # Inicializamos un repositorio falso limpio para cada prueba
+        self.repo_falso = ProveedorRepositoryFalso()        
+        self.service = ProveedorService(self.repo_falso)
+        
+        # Agregar un proveedor base para usar en búsquedas/actualizaciones
+        self.proveedor_base = self.service.registrar_proveedor("Proveedor Central", "2222-3333", "contacto@central.com")
         
     # pruebas de los metodos
-
+    
+    # Pruebas de Validacion
+    def test_validar_nombre_correcto(self):
+        nombre_valido = "   Proveedor ABC   "
+        resultado = self.service.validar_nombre(nombre_valido)
+        self.assertEqual(resultado, "Proveedor ABC")
 
 if __name__ == '__main__':
     unittest.main()
